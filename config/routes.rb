4e5580bf192400ccb3d238b323c604ec7b893de3
Thrishan Anbazhagan <im3shn@gmail.com>
2024-06-22 10:22:27 +0530

    added avatar in appearance page

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-        sessions: 'users/sessions'
-      }
-    end
+        sessions: 'users/sessions',
+        registrations: 'users/registrations'
+  }
   root 'dashboard#index'
   get 'dashboard', to:'dashboard#index'
+  get 'appearance', to: 'dashboard#appearance'
   get ':id', to: 'dashboard#show', as: :user
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
