f3ba4dbeaf12c7f70c22507dd514bb3ace44f49b
Thrishan Anbazhagan <im3shn@gmail.com>
2024-06-24 11:04:36 +0530

    added model validations with username restrictions

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :avatar
+
+  validates :fullname, length: { maximum: 30 }
+  validates :body, length: { maximum: 100 }
+  validate :valid_username
+
+  def valid_username
+    errors.add(:username, "is already taken") if User.exists?(username: username)
+
+    restricted_username_list = %[admin, root, dashboard, appearance]
+
+    errors.add(:username, "is restricted") if restricted_username_list.include?(username)
+  end
+
 end
